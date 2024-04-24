d95564d6deb29c48db509e14de420ccadddc3074
varshaver12 <varshaver12@yandex.ru>
2024-04-24 15:11:33 +0300

    add grpc-swift

--- Package.swift
+++ Package.swift
@@ -4,20 +4,32 @@
 import PackageDescription
 
 let package = Package(
-    name: "FinamAPISwiftSDK",
+    name: "finam-api-swift-sdk",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "FinamAPISwiftSDK",
-            targets: ["FinamAPISwiftSDK"]),
+            targets: ["FinamAPISwiftSDK"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.20.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FinamAPISwiftSDK"),
+            name: "FinamAPISwiftSDK",
+            dependencies: [
+                .product(name: "GRPC", package: "grpc-swift"),
+            ]
+        ),
         .testTarget(
             name: "FinamAPISwiftSDKTests",
             dependencies: ["FinamAPISwiftSDK"]),
     ]
 )
+
