be6cad7963a56572ef9d3740839faa4e73daf587
varshaver12 <varshaver12@yandex.ru>
2024-04-24 20:31:25 +0300

    Сгенерировал контракты

---
+++ generate_protos.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#PROTO_PATH="./Proto"
+#GENERATED_PATH="./Sources/FinamAPISwiftSDK/generated"
+
+#mkdir -p $GENERATED_PATH
+
+# Compile Protobuf files
+#for proto_file in $PROTO_PATH/grpc/tradeapi/v1/*.proto $PROTO_PATH/proto/tradeapi/v1/*.proto; do
+#    protoc \
+#        --proto_path=$PROTO_PATH \
+#        --swift_out=$GENERATED_PATH \
+#        --grpc-swift_out=$GENERATED_PATH \
+#        $proto_file
+#done
+
