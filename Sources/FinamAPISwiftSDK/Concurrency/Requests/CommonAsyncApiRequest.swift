b9ba4e30770ad839b93cf166a3bd4078a13dddaa
varshaver12 <varshaver12@yandex.ru>
2024-04-25 23:33:27 +0300

    Асинхронные вызовы

--- Sources/FinamAPISwiftSDK/Concurrency/Requests/CommonAsyncApiRequest.swift
+++ Sources/FinamAPISwiftSDK/Concurrency/Requests/CommonAsyncApiRequest.swift
@@ -3,21 +3,10 @@ import Foundation
 
 @available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
 public final class CommonAsyncApiRequest<Result>: AsyncApiRequest<CommonApiClient, Result> {
- 
+    
     // MARK: Сервис работы с торговыми поручениями.
-
-    public static func postOrder(
-        clientID: String,
-        securityBoard: String,
-        securityCode: String,
-        buySell: OrderDirection,
-        quantity: Int32,
-        useCredit: Bool,
-        price: Double?,
-        property: OrderProperty,
-        condition: OrderCondition?,
-        validBefore: OrderValidBefore?
-    ) -> CommonAsyncApiRequest<OrderInfo> {
+    
+    public static func postOrder(clientID: String, securityBoard: String, securityCode: String, buySell: BuySell, quantity: Int32, useCredit: Bool, price: Double?, property: OrderProperty, condition: OrderCondition?, validBefore: OrderValidBefore?) -> CommonAsyncApiRequest<NewOrderResult> {
         .init {
             try await $0.orders.postOrder(clientID: clientID,
                                           securityBoard: securityBoard,
@@ -32,15 +21,30 @@ public final class CommonAsyncApiRequest<Result>: AsyncApiRequest<CommonApiClien
         }
     }
     
+    public static func cancelOrder(clientID: String, transactionID: Int32) -> CommonAsyncApiRequest<CancelOrderResult> {
+        .init {
+            try await $0.orders.cancelOrder(clientID: clientID,
+                                            transactionID: transactionID)
+        }
+    }
+    
+    public static func getOrders(clientID: String, includeMatched: Bool, includeCanceled: Bool, includeActive: Bool) -> CommonAsyncApiRequest<GetOrdersResult> {
+        .init {
+            try await $0.orders.getOrders(clientID: clientID,
+                                          includeMatched: includeMatched,
+                                          includeCanceled: includeCanceled,
+                                          includeActive: includeActive)
+        }
+    }
+    
+    
     // MARK: Сервис получения списка инструментов.
- 
-    public static func getSecurities(
-        board: String?,
-        seccode: String?
-    ) -> CommonAsyncApiRequest<[Security]> {
+    
+    public static func getSecurities(board: String?, seccode: String?) -> CommonAsyncApiRequest<[Security]> {
         .init {
             try await $0.securities.getSecurities(board: board, seccode: seccode)
         }
     }
 }
 #endif
+
